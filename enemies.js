2fd8d9a9b8622d6041c89d1390cb9c24d4a14b69
E-Chiu <31664636+E-Chiu@users.noreply.github.com>
2017-10-27 10:45:57 -0600

    enemies

---
+++ enemies.js
@@ -0,0 +1,13 @@
+class Enemy {
+    constructor(color, size, speed, damage, range, attackCd) {
+        this.color = color;
+        this.size = size;
+        this.speed = speed;
+        this.damage = damage;
+        this.range = range;
+        this.atkCd = attackCd;
+        this.actualCd = attackCd;
+    }
+    
+    function1 (){}
+}
